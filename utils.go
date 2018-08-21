43fa8149c8718033b6cff02810407fb75f02ff86
rdorrigan <31552969+rdorrigan@users.noreply.github.com>
2018-08-21 14:01:24 -0700

    Add files via upload

---
+++ utils.go
@@ -0,0 +1,45 @@
+package fileDateSort
+
+import (
+	"os"
+	"time"
+)
+
+// File is a file
+type File struct {
+	Info os.FileInfo
+}
+
+// Fldr is a folder
+type Fldr struct {
+	Files []File
+}
+
+// Less Checks the i is Before j
+func (f Fldr) Less(i, j int) bool {
+	// x := f.Files[i].Info.ModTime
+	// y := f.Files[j].Info.ModTime
+	// z := first.Before(y)
+	return f.Files[i].Info.ModTime().Before(f.Files[j].Info.ModTime())
+}
+
+// Swap swaps files in a Fldr slice
+func (f *Fldr) Swap(i, j int) {
+	f.Files[i], f.Files[j] = f.Files[j], f.Files[i]
+}
+
+// Latest returns the most recently modified file in a folder
+func (f Fldr) Latest() os.FileInfo {
+	var modTime time.Time
+	var names []File
+	for _, i := range f.Files {
+		if !i.Info.ModTime().Before(modTime) {
+			if i.Info.ModTime().After(modTime) {
+				modTime = i.Info.ModTime()
+				names = names[:0]
+			}
+			names = append(names, i)
+		}
+	}
+	return names[0].Info
+}
